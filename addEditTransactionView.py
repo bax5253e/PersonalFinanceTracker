8415452d824a673aa9d650f766dff1edb3d5675f
Alberto Botero <bax5253e@cpcc.edu>
2024-12-03 20:20:54 -0500

    Adding User Interface

---
+++ addEditTransactionView.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+class AddEditTransactionView(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.grid(row=0, column=0, sticky="nsew")
+
+        tk.Label(self, text="Add/Edit Transaction", font=("Arial", 18)).pack(pady=20)
+        tk.Button(self, text="Go Back", font=("Arial", 16), command=lambda: controller.show_frame("MainView")).pack(pady=10)
